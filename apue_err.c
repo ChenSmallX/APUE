edaf18bec27f234a123e69465f0d4b553e8f53d6
ChenSmallX <641751205@qq.com>
2020-06-22 13:50:09 +0800

    error output with apue.h

--- apue_err.c
+++ apue_err.c
@@ -1,6 +1,6 @@
 #include "apue.h"
-#include <errno.h>
-#include <stdarg.h>
+#include <errno.h>      /* for definition of errno */
+#include <stdarg.h>     /* ISO C variable arguments */
 
 static void err_doit(int, int, const char *, va_list);
 
